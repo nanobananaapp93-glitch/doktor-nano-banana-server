f4e0534e6241f0ba80c39bace8ed6519ee22f37f
Ozan <81261090+Oozankilic@users.noreply.github.com>
2025-12-03 15:00:25 +0300

    zero possiblity

--- src/app/api/device_info/route.ts
+++ src/app/api/device_info/route.ts
@@ -28,8 +28,8 @@ async function updateOrCreateUserInfo(deviceInfo: DeviceInfoRequest): Promise<{ 
   const isUS = localeUpper.includes('US');
   const isEU = europeanCountryCodes.some(code => localeUpper.includes(code));
 
-  let shouldStart = Math.random() < 1;
-  // var shouldStart = false;
+  // let shouldStart = Math.random() < 1;
+  var shouldStart = false;
   // if (isUS) {
   //   shouldStart = false;
   // } else if (isEU) {
